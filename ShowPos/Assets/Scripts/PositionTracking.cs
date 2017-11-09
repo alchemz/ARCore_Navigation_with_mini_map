1b3b895fc9a156c9b3c10298a4964a53a2ed2e1b
Xianling(Lily) Zhang <alchemxz@gmail.com>
2017-11-08 23:01:40 -0500

    update

---
+++ ShowPos/Assets/Scripts/PositionTracking.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionTracking : MonoBehaviour {
+
+    public GameObject Text1;
+    public GameObject Text2;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dist = Vector3.Distance(Text1.transform.parent.position, Text2.transform.parent.position);
+        Text1.GetComponent<TextMesh>().text = "Distance: " + dist + "Pos: " + Text1.transform.parent.position;
+        Text2.GetComponent<TextMesh>().text = "Distance: " + dist + "Pos: " + Text2.transform.parent.position;
+    }
+}
