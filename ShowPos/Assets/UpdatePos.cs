79f0cd05c048d4e73c0933c4702aa91f759977c9
Xianling(Lily) Zhang <alchemxz@gmail.com>
2017-11-13 20:33:24 -0500

    update

---
+++ ShowPos/Assets/UpdatePos.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdatePos : MonoBehaviour {
+    public GameObject trackedObject;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(trackedObject.transform.position.x, 6.0f, trackedObject.transform.position.z);
+        transform.eulerAngles = new Vector3(90, 0, trackedObject.transform.eulerAngles.z);
+	}
+}
